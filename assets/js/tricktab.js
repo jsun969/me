074719bb93840aa2516e7216f9e862e19e0f7a1a
jsun969 <sunjustin969@gmail.com>
2021-02-18 12:04:30 +0800

    feat: add main files

---
+++ assets/js/tricktab.js
@@ -0,0 +1,16 @@
+// 浏览器标题切换  
+var OriginTitile = document.title;    // 保存之前页面标题  
+var titleTime;  
+document.addEventListener('visibilitychange', function(){  
+    if (document.hidden){  
+        document.title = '看这里!看这里!';  
+        titleTime = setTimeout(function() {  
+            document.title = OriginTitile;  
+        }, 3000); // 3秒后恢复原标题   
+    }else{  
+        document.title = '你回来啦!';  
+        titleTime = setTimeout(function() {  
+            document.title = OriginTitile;  
+        }, 500); // 0.5秒后恢复原标题  
+    }  
+});  
